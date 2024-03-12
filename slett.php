7df1f47d78ebd57a7c1cb6afb8564b0a4430c87f
Mummitrollet12 <sondre.ringhus@gmail.com>
2024-03-12 09:40:17 +0100

    g

--- slett.php
+++ slett.php
@@ -31,3 +31,32 @@
                   </tr>
             </table>
     </section>
+
+
+
+
+
+<?php
+// Henter forbindelses-streng
+include 'connect.php';
+
+// Sjekk om skjemaet er sendt
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Hent personens ansattnummer fra skjemaet
+  $anr_to_slett = mysqli_real_escape_string($conn, $_POST['anr']);
+
+  // SQL-spørring for å slette personen
+  $sql_slett = "DELETE FROM person WHERE anr = '$anr_to_slett'";
+
+  // Utfør sletteoperasjonen
+  if (mysqli_query($conn, $sql_slett)) {
+    echo "Personen er slettet.<br>";
+    echo "<a href='index.php'>Tilbake</a><br>";
+  } else {
+    echo "Feil ved sletting av person: " . mysqli_error($conn);
+  }
+}
+
+// Lukk databaseforbindelsen
+mysqli_close($conn);
+?>
